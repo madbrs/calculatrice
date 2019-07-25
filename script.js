5169cfeafcb111cdd32c931614e416582ce76891
thenativewolf <dbrsma@gmail.com>
2019-07-25 17:15:10 +0200

    first commitsecond

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-// déclarer les boutons
-// faire les fonctions
 
-let number
-let operation
-let equal
- let delate
- let allclear
+
+function btn(value) {
+    document.getElementById("result").innerHTML += value;
+}
+function btnClear(){
+   document.getElementById("result").innerHTML = "";
+}
+function btnPlus() {
+   document.getElementById("result").innerHTML += "+";
+}
+function btnLess() {
+   document.getElementById("result").innerHTML += "-";
+}
+function btnMulti() {
+   document.getElementById("result").innerHTML += "*";
+}
+function btnDiv() {
+   document.getElementById("result").innerHTML += "/";
+}
+
+function btnEqual() {
+     let egal = eval(document.getElementById('result').innerHTML);
+     document.getElementById('result').innerHTML = egal;
+ }
+
+// function pour virgule
+// function pour retour
+
